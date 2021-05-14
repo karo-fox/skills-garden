d395f3a384c9caae7fb37b8339448e0945eb8ca6
Karolina <k4aro0@gmail.com>
2021-05-14 15:09:56 +0200

    creating models

--- sg_project/garden/models.py
+++ sg_project/garden/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Field(models.Model):
+    name = models.CharField(max_length=100)
+    # user = models.ForeignKey(User)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
+    last_reviewed = models.DateField()
+    review_frequency = models.DurationField()
+
+    def __str__(self):
+        return f'"{self.name}" in field "{self.field}"'

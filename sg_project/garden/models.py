778a2251165f6180574c532971dc1aaa28faf5c5
Karolina <k4aro0@gmail.com>
2021-05-17 15:33:11 +0200

    garden models changed

--- sg_project/garden/models.py
+++ sg_project/garden/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Field(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField()
     # user = models.ForeignKey(User)
     date_added = models.DateField(auto_now_add=True)
+    last_reviewed = models.DateField()
+    review_frequency = models.DurationField()
 
     def __str__(self):
         return self.name
@@ -11,11 +14,10 @@ class Field(models.Model):
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
-    text = models.TextField()
+    description = models.TextField()
     field = models.ForeignKey(Field, on_delete=models.CASCADE)
     date_added = models.DateField(auto_now_add=True)
     last_reviewed = models.DateField()
-    review_frequency = models.DurationField()
 
     def __str__(self):
         return f'"{self.name}" in field "{self.field}"'

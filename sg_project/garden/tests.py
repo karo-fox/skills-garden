1ac808b72ad2810705a857e6d105cf265a1af293
Karolina <k4aro0@gmail.com>
2021-05-19 13:44:19 +0200

    tests for fiel view added

--- sg_project/garden/tests.py
+++ sg_project/garden/tests.py
@@ -31,10 +31,10 @@ class TestModels(TestCase):
     
 
     def test_field_str(self):
-        self.assertEqual(self.field1.__str__(), 'Field: Test field - 14 days, 0:00:00 - 2021-05-18')
+        self.assertEqual(self.field1.__str__(), 'Test field')
 
     def test_topic_str(self):
-        self.assertEqual(self.topic1.__str__(), 'Topic: Test topic - Test field - 2021-05-18')
+        self.assertEqual(self.topic1.__str__(), 'Test topic')
 
 
 
@@ -49,11 +49,29 @@ class TestUrls(SimpleTestCase):
 
 
 
-class TestView(TestCase):
+class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.urlconf = 'garden.urls'
+
+        date_added = datetime.date(2021, 4, 1)
+        last_reviewed = datetime.date(2021, 5, 18)
+        review_frequency = datetime.timedelta(14)
+        self.field1 = Field.objects.create(
+            name='Test field',
+            description='test test test',
+            date_added=date_added,
+            last_reviewed=last_reviewed,
+            review_frequency=review_frequency
+        )
+        self.topic1 = Topic.objects.create(
+            name='Test topic',
+            description='test test test',
+            date_added=date_added,
+            last_reviewed=last_reviewed,
+            field=self.field1
+        )
     
     def test_home_view(self):
         response = self.client.get(reverse('home', urlconf=self.urlconf))
@@ -61,3 +79,11 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
         self.assertQuerysetEqual(response.context['object_list'], Field.objects.all())
+
+
+    def test_field_view(self):
+        response = self.client.get(reverse('field', args=[1], urlconf=self.urlconf))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'field.html')
+        self.assertQuerysetEqual(response.context['topic_list'], Topic.objects.filter(field=self.field1))

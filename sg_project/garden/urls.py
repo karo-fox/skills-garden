69b551c887f87e588b83c3196e08e3b9576daca2
Karolina <k4aro0@gmail.com>
2021-05-19 13:29:54 +0200

    get context data for field view added

--- sg_project/garden/urls.py
+++ sg_project/garden/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, FieldView
 
 app_name = 'garden'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('home/', HomeView.as_view(), name='home')
+    path('home/', HomeView.as_view(), name='home'),
+    path('field/<int:pk>', FieldView.as_view(), name='field')
 ]
--- sg_project/garden/views.py
+++ sg_project/garden/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Field, Topic
 
 class HomeView(ListView):
     template_name = 'index.html'
-    queryset = Field.objects.all()
+    model = Field
+    context_object_name = 'field_list'
 
 
-class FieldView(ListView):
+class FieldView(DetailView):
     template_name = 'field.html'
+    model = Field
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topic_list'] = Topic.objects.filter(field_id=context['object'].id)
+        return context

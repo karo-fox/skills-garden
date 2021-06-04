79a0d4a0ddfe507bc84a150d7fec49a02a41d32c
Karolina <k4aro0@gmail.com>
2021-06-04 08:28:08 +0200

    front: field view template implemented

--- sg_project/client/static/scripts/tab.js
+++ sg_project/client/static/scripts/tab.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-  createList('/garden/fields/');
+function tab(url) {
+  createList(url);
   $(".tab-item").on("click", function(e) {
     $(this).addClass("is-active").siblings().removeClass("is-active");
     if(this.id === "list") {
-      createList('/garden/fields/');
+      createList(url);
     } else if(this.id === "create") {
       $('.main-section').html('<p>Create Form</p>');
     } else if(this.id === "journal") {
@@ -12,10 +12,9 @@ $(document).ready(function() {
       console.log("none");
     }
   });
-});
+}
 
 function createList(url) {
-  let objects = []
   $.getJSON(url, function(data) {
     objects = data.map(function(item) {
       return `<div class="block"><a href="${item.url}"><button class=" level button is-black is-fullwidth"><div class="level-left">${item.name}</div><div class="left-right">${item.last_reviewed}</div></button></a></div>`

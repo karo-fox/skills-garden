c94a6abc8fa6d89a8644aece57461ad19fe02431
Karolina <k4aro0@gmail.com>
2021-08-13 10:31:13 +0200

    garden: models edited

--- sg_project/garden/models.py
+++ sg_project/garden/models.py
@@ -21,9 +21,6 @@ class Field(models.Model):
     def __repr__(self):
         return f'models.Field({self.name}, {self.description}, {self.date_added}, {self.last_reviewed}, {self.review_frequency})'
 
-    # def admin_topic_filter_link(self):
-    #     return mark_safe(f'<a href="/admin/garden/topic/?field__id__exact={self.id}">topics</a>')
-
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
@@ -33,13 +30,10 @@ class Topic(models.Model):
     last_reviewed = models.DateField(default=None, null=True)
 
     def get_absolute_url(self):
-        return reverse('garden:topic-detail', kwargs={'field_pk': self.field.id, 'pk': self.id})
+        return reverse('garden:topic-detail', kwargs={'field_pk': self.field.pk, 'pk': self.pk})
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return f'models.Topic({self.name}, {self.description}, {self.field}, {self.date_added}, {self.last_reviewed})'
-
-    # def admin_field_link(self):
-    #     return mark_safe(f'<a href="/admin/garden/field/{self.field.id}/change/">{self.field.name}</a>')
